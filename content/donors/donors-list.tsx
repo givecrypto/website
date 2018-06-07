14fb07203fa9daa92a8f58a086af5feea6e9e14f
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-06-06 20:06:26 -0400

    removes all people form donors page

--- content/donors/donors-list.tsx
+++ content/donors/donors-list.tsx
@@ -3,66 +3,66 @@ export const top = [
     name: 'Brian Armstrong',
     role: 'CEO, Coinbase',
     filename: 'brian-armstrong'
-  },
-  {
-    name: 'Rose Broome',
-    role: 'Founder, HandUp.org',
-    filename: 'rose-broome'
-  },
-  {
-    name: 'Jake LeBoeuf',
-    role: 'Software Engineer',
-    filename: 'jake-leboeuf'
   }
+  // {
+  //   name: 'Rose Broome',
+  //   role: 'Founder, HandUp.org',
+  //   filename: 'rose-broome'
+  // },
+  // {
+  //   name: 'Jake LeBoeuf',
+  //   role: 'Software Engineer',
+  //   filename: 'jake-leboeuf'
+  // }
 ];
 
 export const middle = [
-  'Dan Gilber',
-  'Amy Roulp',
-  'Amy Roulp',
-  'Wedge Taylor',
-  'Anthony Rouse',
-  'Melony Sou',
-  'Silver Danse',
-  'Lima Rou',
-  'Bilba Nounset',
-  'Eb Washingmont',
-  'Sara Tarsik',
-  'Stan Lebru'
+  // 'Dan Gilber',
+  // 'Amy Roulp',
+  // 'Amy Roulp',
+  // 'Wedge Taylor',
+  // 'Anthony Rouse',
+  // 'Melony Sou',
+  // 'Silver Danse',
+  // 'Lima Rou',
+  // 'Bilba Nounset',
+  // 'Eb Washingmont',
+  // 'Sara Tarsik',
+  // 'Stan Lebru'
 ];
 
 export const bottom = [
-  'Melony Sou',
-  'Dan Gilber',
-  'Amy Roulp',
-  'Melony Sou',
-  'Melony Sou',
-  'Melony Sou',
-  'Melony Sou',
-  'Wedge Taylor',
-  'Anthony Rouse',
-  'Melony Sou',
-  'Silver Danse',
-  'Anthony Rouse',
-  'Wedge Taylor',
-  'Anthony Rouse',
-  'Melony Sou',
-  'Silver Danse',
-  'Anthony Rouse',
-  'Wedge Taylor',
-  'Anthony Rouse',
-  'Melony Sou',
-  'Silver Danse',
-  'Anthony Rouse',
-  'Wedge Taylor',
-  'Anthony Rouse',
-  'Melony Sou',
-  'Anthony Rouse',
-  'Eb Washingmont',
-  'Sara Tarsik',
-  'Sara Tarsik',
-  'Sara Tarsik',
-  'Stan Lebru'
+  // 'Melony Sou',
+  // 'Dan Gilber',
+  // 'Amy Roulp',
+  // 'Melony Sou',
+  // 'Melony Sou',
+  // 'Melony Sou',
+  // 'Melony Sou',
+  // 'Wedge Taylor',
+  // 'Anthony Rouse',
+  // 'Melony Sou',
+  // 'Silver Danse',
+  // 'Anthony Rouse',
+  // 'Wedge Taylor',
+  // 'Anthony Rouse',
+  // 'Melony Sou',
+  // 'Silver Danse',
+  // 'Anthony Rouse',
+  // 'Wedge Taylor',
+  // 'Anthony Rouse',
+  // 'Melony Sou',
+  // 'Silver Danse',
+  // 'Anthony Rouse',
+  // 'Wedge Taylor',
+  // 'Anthony Rouse',
+  // 'Melony Sou',
+  // 'Anthony Rouse',
+  // 'Eb Washingmont',
+  // 'Sara Tarsik',
+  // 'Sara Tarsik',
+  // 'Sara Tarsik',
+  // 'Stan Lebru'
 ];
 
 export default { top, middle, bottom };
