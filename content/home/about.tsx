18b4c1dc72838dc5d5cf46b2e12af3ed1ebe524f
Brian Armstrong <brian@coinbase.com>
2018-06-12 16:20:53 -0700

    Update about.tsx

--- content/home/about.tsx
+++ content/home/about.tsx
@@ -1,10 +1,10 @@
 export default {
   title: `Our mission is to financially empower people by distributing cryptocurrency globally.`,
   sectionOne: `Two billion people have smartphones, but don't have access to basic financial services.`,
-  sectionTwo: `Gifting cryptocurrency to people in need connects them to an open financial system.`,
+  sectionTwo: `We give cryptocurrency to people to help connect them to an open financial system.`,
   sectionThree: `
-Our first steps to achieving this:
-- Cash out to their local currency
+Recipients can:
+- Cash out crypto to their local currency
 - Buy goods and services they need 
 - Transact in crypto or hold it long term 
 `
