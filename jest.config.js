65f8470c5f321f8caed96ae6449228ea38cda4c5
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-06-12 09:45:00 -0400

    fixes test on index page

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   setupFiles: ['<rootDir>/jest.setup.tsx'],
+
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)(spec|test).ts?(x)'],
