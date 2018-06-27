aebe3acccce980528fc0df09cd6e50d560d3e18e
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-06-27 12:37:28 -0400

    udpates video link

--- content/home/video-section.tsx
+++ content/home/video-section.tsx
@@ -4,5 +4,5 @@ export default {
   subtitle: `“Given the enormous wealth creation from cryptocurrency, and the future potential upside, I believe there is a rare opportunity to create a large non-profit fund.”`,
   meta: `- Brian Armstrong, Founder`,
   link:
-    'https://medium.com/@barmstrong/charitable-giving-with-cryptocurrency-11e24923cd4a'
+    'https://medium.com/givecrypto/introducing-givecrypto-org-fce707da03ee'
 };
