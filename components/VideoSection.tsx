ae9490063f9f1fafe89ad68c0f629bd1fbc964ef
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-07-28 10:38:42 -0400

    adds promo video to home page

--- components/VideoSection.tsx
+++ components/VideoSection.tsx
@@ -18,16 +18,16 @@ const LinkContainer = glamorous.h2({
     fontWeight: 800,
 
     '&:hover': {
-      color: colors.blueDark
-    }
-  }
+      color: colors.blueDark,
+    },
+  },
 });
 
 const Title = glamorous.h2({
   fontSize: Step(6),
   textAlign: 'center',
   paddingBottom: Step(5),
-  color: colors.black
+  color: colors.black,
 });
 
 const Subtitle = glamorous.h3({
@@ -35,31 +35,35 @@ const Subtitle = glamorous.h3({
   fontSize: Step(5),
   fontStyle: 'italic',
   fontWeight: 400,
-  color: colors.greyLight
+  color: colors.greyLight,
 });
 
 const Meta = glamorous.h4({
   fontSize: Step(5),
   fontWeight: 900,
-  color: colors.black
+  color: colors.black,
 });
 
 const VideoSection: React.SFC<VideoSectionProps> = () => {
+  const { title, promoId, youtubeId, subtitle, meta, link } = content;
   return (
     <div className="center tc">
       <Title>
-        <ReactMarkdown source={content.title} />
+        <ReactMarkdown source={title} />
       </Title>
       <Wrapper narrow className="mb6">
-        <VideoPlayer videoId={content.youtubeId} />
+        <VideoPlayer videoId={youtubeId} />
       </Wrapper>
       <Subtitle className="lh-copy measure center">
-        <ReactMarkdown source={content.subtitle} />
+        <ReactMarkdown source={subtitle} />
       </Subtitle>
-      <Meta className="lh-title">{content.meta}</Meta>
+      <Meta className="lh-title">{meta}</Meta>
       <LinkContainer>
-        <a href={content.link}>Read the story -></a>
+        <a href={link}>Read the story -></a>
       </LinkContainer>
+      <Wrapper narrow className="mt6">
+        <VideoPlayer videoId={promoId} />
+      </Wrapper>
     </div>
   );
 };
