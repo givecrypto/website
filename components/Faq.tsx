aa25f379b752df5bd6c630a2b08fa581cb461ef7
Jake LeBoeuf <dev@jakeleboeuf.com>
2019-06-05 13:40:44 -0400

    adds new faqs, fixes a styling bug and switches to yarn :party:

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,4 +1,4 @@
-import glamorous from "glamorous";
+import styled from "@emotion/styled";
 import * as React from "react";
 import {
   Accordion,
@@ -13,80 +13,86 @@ import { colors } from "../design-system";
 import "../styles/accordion.css";
 import { Step } from "../utils/Scale";
 
-const SubHeading = glamorous.h2({
+const SubHeading = styled.h2({
   fontSize: Step(5),
   fontWeight: 500,
   paddingRight: Step(4.5),
 });
 
-const Faqs: any = props => {
-  return props.qa.map((item: any) => {
-    const Wrapper = glamorous.div({
-      padding: `${Step(5)} 0`,
-      borderBottom: `2px solid ${colors.greyLightest}`,
-      "> div": {
-        overflow: "hidden",
-        outline: "none",
-        "& a": {
-          color: colors.green,
-          "&:hover": {
-            color: colors.white,
-          },
-        },
-        "> h2": {
-          color: props.theme === "light" ? colors.black : colors.white,
-          cursor: "pointer",
-          position: "relative",
-          display: "inline-block",
-          width: "100%",
-          "&:after": {
-            lineHeight: 0,
-            transition: "all 300ms",
-            transform: `rotate(0)`,
-            padding: 0,
-            margin: 0,
-            verticalAlign: "top",
-            fontSize: Step(6),
-            content: "+",
-            color: colors.blue,
-            display: "block",
-            position: "absolute",
-            right: 0,
-            top: `0.85rem`,
-          },
-        },
-        "& p, & li": {
-          fontSize: Step(5),
-          lineHeight: Step(4.95, "none"),
-          color: colors.greyLight,
-        },
-        "&:focus": {
-          "& h2": {
-            color: colors.greyLight,
-          },
-        },
-        "&[aria-expanded=true]": {
-          "& h2": {
-            color: props.theme === "light" ? colors.black : colors.white,
-            // textShadow: '0 0 10px rgba(255,255,255,0.25)',
-            "&:after": {
-              color: colors.red,
-              transform: `rotate(45deg)`,
-            },
-          },
+const Wrapper = styled.div(({ theme }) => ({
+  padding: `${Step(5)} 0`,
+  borderBottom: `2px solid ${colors.greyLightest}`,
+  "> div": {
+    overflow: "hidden",
+    outline: "none",
+    "& a": {
+      color: colors.green,
+      "&:hover": {
+        color: colors.white,
+      },
+    },
+    "> h2": {
+      color: theme === "light" ? colors.black : colors.white,
+      cursor: "pointer",
+      position: "relative",
+      display: "inline-block",
+      width: "100%",
+      "&:after": {
+        lineHeight: 0,
+        transition: "all 300ms",
+        transform: `rotate(0)`,
+        padding: 0,
+        margin: 0,
+        verticalAlign: "top",
+        fontSize: Step(6),
+        content: "+",
+        color: colors.blue,
+        display: "block",
+        position: "absolute",
+        right: 0,
+        top: `0.85rem`,
+      },
+    },
+    "& p, & li": {
+      fontSize: Step(5),
+      lineHeight: Step(4.95, "none"),
+      color: colors.greyLight,
+    },
+    "&:focus": {
+      "& h2": {
+        color: colors.greyLight,
+      },
+    },
+    "&[aria-expanded=true]": {
+      "& h2": {
+        color: theme === "light" ? colors.black : colors.white,
+        // textShadow: '0 0 10px rgba(255,255,255,0.25)',
+        "&:after": {
+          color: colors.red,
+          transform: `rotate(45deg)`,
         },
       },
-    });
+    },
+  },
+}));
+
+const Faqs: any = (props: {
+  qa: { map: (arg0: (item: any) => JSX.Element) => void };
+  theme: string;
+}) => {
+  return props.qa.map((item: any) => {
     return (
       <AccordionItem key={`key-${item.question}`}>
-        <Wrapper>
-          <AccordionItemTitle>
-            <SubHeading className="lh-title">{item.question}</SubHeading>
-          </AccordionItemTitle>
-          <AccordionItemBody>
-            <ReactMarkdown source={item.answer} />
-          </AccordionItemBody>
-        </Wrapper>
+        <div>
+          <Wrapper theme={props.theme}>
+            <AccordionItemTitle>
+              <SubHeading className="lh-title">{item.question}</SubHeading>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+              <ReactMarkdown source={item.answer} />
+            </AccordionItemBody>
+          </Wrapper>
+        </div>
       </AccordionItem>
     );
   });
@@ -102,7 +108,7 @@ export default class Faq extends React.Component<FaqProps, any> {
   public render() {
     const { title = "FAQs", theme = "dark", list = compactFaq } = this.props;
 
-    const Heading = glamorous.h2({
+    const Heading = styled.h2({
       color: theme === "light" ? colors.black : colors.white,
       lor: colors.white,
       fontSize: Step(6),
--- content/faq/compact-faq.tsx
+++ content/faq/compact-faq.tsx
@@ -16,15 +16,49 @@ export default [
     answer: `We've made the commitment that, for now, 100% of donated funds will go to recipients. GiveCrypto.org’s operating overhead will be covered by founding members for right now. If you're making a donation of $25k or more and are comfortable with GiveCrypto using your donation for operating overhead in addition to distributions to recipients, please send us a note and let us know.`,
   },
   {
-    question: `How do the funds get distributed to people in need?`,
-    answer: `We will be testing out a number of distribution strategies over time. To get started, we're traveling to regions ourselves to distribute funds in person. We're also speaking with a handful of nonprofits who could act as distribution partners (organizations with local people operating in markets that we are interested in reaching). Finally, we may build a decentralized application in the future to create a trust network for distributions. Recipients can save or spend their crypto, or convert it into cash using local exchanges.`,
-  },
-  {
     question: `How will the funds help people?`,
     answer: `There is substantial [evidence](https://www.givewell.org/international/technical/programs/cash-transfers) that unconditional cash transfers are an effective way to help people. Distributing crypto is similar to transferring other forms of money to people. We expect many recipients will find ways to convert the crypto into their local currency. Some may transact crypto-to-crypto as well. It may also help spark economic growth by giving access to property rights and financial services on an open network. A great deal of work needs to be done to test these hypotheses and see what it most effective.`,
   },
   {
     question: `How can I help?`,
     answer: `[Donate now](/donate) to our initial fund. You can also help us get the word out by sharing this website with your friends and subscribing to our blog to receive future updates.`,
+  },
+  {
+    question: "How will you hold the funds?",
+    answer: `We want to maximize our exposure to crypto’s potential appreciation and will hold as much crypto as possible. Obviously, we will need to hold some cash to pay salaries and operational expenses.`,
+  },
+  {
+    question: "Why is crypto better than fiat?",
+    answer: `While cryptocurrency is somewhat difficult to use (for now) and isn’t accepted by many vendors (yet), it does have some benefits compared to fiat currencies. These revolve around its relative easy and low cost transfer (especially across international borders) as well as the visibility into downstream activity.`,
+  },
+  {
+    question: "Can donors determine who gets the funds?",
+    answer:
+      "No. We rely on our network of ambassadors to determine who receives our funds. The ambassadors have deep local understanding and will determine who is in the most need.",
+  },
+  {
+    question: "How much do recipients get?",
+    answer:
+      "We have experimented with different donation sizes, durations and frequencies. We are currently giving participants $10 per week for 6 weeks.",
+  },
+  {
+    question: "Are the transfers one time or ongoing?",
+    answer:
+      "All of our projects to date have had a specific start and end date. We don’t yet have any plans to implement an open ended distribution project.",
+  },
+  {
+    question: "Why do you operate in Venezuela?",
+    answer:
+      "We decided to focus solely on Venezuela due to their economic situation. We feel that we can have an outsized impact in Venezuela and that our chances of sparking a cryptoeconomy are highest in Venezuela. ",
+  },
+  {
+    question: "How do you prevent fraud?",
+    answer:
+      "We use a combination of data collection validated with backchecks, verifiable evidence and blockchain analysis to minimize and prevent fraud. ",
+  },
+  {
+    question: "Where do recipients use the funds?",
+    answer:
+      "We recruit at least one vendor that accepts payment in cryptocurrency. Additionally, recipients hold the crypto and exchange it between themselves. In the future, we will offer cash-out options.",
   },
 ];
