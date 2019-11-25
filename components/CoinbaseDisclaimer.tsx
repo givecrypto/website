dd95187fc2d339ee8251fa363619117c3931ccf8
Jake LeBoeuf <dev@jakeleboeuf.com>
2019-11-25 13:49:17 -0500

    fine, mark- less lines

--- components/CoinbaseDisclaimer.tsx
+++ components/CoinbaseDisclaimer.tsx
@@ -3,23 +3,18 @@ import * as React from "react";
 import { colors } from "../design-system";
 import { Step } from "../utils/Scale";
 
-const Container = glamorous.section({
+const Title = glamorous.p({
   padding: `${Step(1)} 0`,
   maxWidth: 320,
-});
-const Title = glamorous.p({
-  maxWidth: 600,
   fontSize: Step(4),
   color: colors.grey,
 });
 
 const CoinbaseDisclaimer: React.SFC = () => {
   return (
-    <Container className="tc center">
-      <Title className="lh-copy center">
-        GiveCrypto.org is a non-profit entity unaffiliated with Coinbase.
-      </Title>
-    </Container>
+    <Title className="lh-copy center">
+      GiveCrypto.org is a non-profit entity unaffiliated with Coinbase.
+    </Title>
   );
 };
 
