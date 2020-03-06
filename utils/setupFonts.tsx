a62f429e7a4c2fb6c991e76f3657c2dfa9c27e9b
Jake LeBoeuf <dev@jakeleboeuf.com>
2020-03-06 11:50:30 -0500

    oh em gee

--- utils/setupFonts.tsx
+++ utils/setupFonts.tsx
@@ -2,58 +2,61 @@ import { injectGlobal } from "emotion";
 
 // Mono
 injectGlobal`
+  * {
+    font-family: "Apercu";
+  }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: normal;
-    fontWeight: 400;
+    font-family: "Apercu";
+    font-style: normal;
+    font-weight: 400;
     src: url('/static/fonts/Apercu-Light.woff2') format('woff2'),
     url('/static/fonts/Apercu-Light.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: normal;
-    fontWeight: 200;
+    font-family: "Apercu";
+    font-style: normal;
+    font-weight: 200;
     src: url('/static/fonts/Apercu-Mono.woff2') format('woff2'),
       url('/static/fonts/Apercu-Mono.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: normal;
-    fontWeight: 500;
+    font-family: "Apercu";
+    font-style: normal;
+    font-weight: 500;
     src: url('/static/fonts/Apercu-Regular.woff2') format('woff2'),
       url('/static/fonts/Apercu-Regular.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: normal;
-    fontWeight: 700;
+    font-family: "Apercu";
+    font-style: normal;
+    font-weight: 700;
     src: url('/static/fonts/Apercu-Medium.woff2') format('woff2'),
       url('/static/fonts/Apercu-Medium.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: normal;
-    fontWeight: 900;
+    font-family: "Apercu";
+    font-style: normal;
+    font-weight: 900;
     src: url('/static/fonts/Apercu-Bold.woff2') format('woff2'), url('/static/fonts/Apercu-Bold.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: "italic";
-    fontWeight: 500;
+    font-family: "Apercu";
+    font-style: "italic";
+    font-weight: 500;
     src: url('/static/fonts/Apercu-Italic.woff2') format('woff2'),
       url('/static/fonts/Apercu-Italic.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: "italic";
-    fontWeight: 700;
+    font-family: "Apercu";
+    font-style: "italic";
+    font-weight: 700;
     src: url('/static/fonts/Apercu-MediumItalic.woff2') format('woff2'),
       url('/static/fonts/Apercu-MediumItalic.woff') format('woff');
   }
   @font-face {
-    fontFamily: "Apercu";
-    fontStyle: "italic";
-    fontWeight: 900;
+    font-family: "Apercu";
+    font-style: "italic";
+    font-weight: 900;
     src: url('/static/fonts/Apercu-BoldItalic.woff2') format('woff2'),
       url('/static/fonts/Apercu-BoldItalic.woff') format('woff');
   }
